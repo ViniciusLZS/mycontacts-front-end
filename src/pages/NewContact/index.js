2c48f396021af8b8ea92c6d718a9dcc1b5b5ce01
Vinícius <vinicius.luiz.9256@gmail.com>
2023-04-25 15:00:05 -0300

    feat: handleSubmit function

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,7 +1,21 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  const handleSubmit = async (formData) => {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+      const response = await ContactsService.createContact(contact);
+      console.log('🚀 ~ file: index.js:14 ~ handleSubmit ~ response:', response);
+    } catch { alert('Ocorreu um erro ao cadastrar'); }
+  };
+
   return (
     <>
       <PageHeader
@@ -9,6 +23,7 @@ export default function NewContact() {
       />
       <ContactForm
         buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
       />
     </>
   );
