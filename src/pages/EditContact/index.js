c7fbe6f3076d6cd300b0bff175dcd324a3a93bf9
Vinícius <vinicius.luiz.9256@gmail.com>
2023-06-06 17:50:22 -0300

    feat: contact update

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,14 +1,77 @@
+import { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+
+import Loader from '../../components/Loader';
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFielsValues(contact);
+
+        setIsLoading(false);
+        setContactName(contact.name);
+      } catch (error) {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado',
+        });
+      }
+    }
+    loadContact();
+  }, [id, history]);
+
+  const handleSubmit = async (formData) => {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+      const contactData = await ContactsService.updateContact({ id, contact });
+
+      setContactName(contactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+        duration: 5000,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar um contato!',
+      });
+    }
+  };
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
-        title="Editar Vinícius"
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
       />
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
 
